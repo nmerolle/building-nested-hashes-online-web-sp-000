52512ab3d1475b7ede1100115a67d99b48f3e99c
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-09 23:20:07 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,7 +17,7 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet", :age "50"
+        :name => "Lord Capulet", :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
