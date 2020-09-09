ee6e597da7b5286f7f920a4cf38cc26967c4a9cf
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-09 23:15:59 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,7 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {:name => "Lord Montague", :age => "53"},
+      :patriarch => {},
       :matriarch => {:name => "Lady Montague", :age => "54"},
       :hero => {:name => "Romeo", :age => "15", :status => "alive"},
       :hero_friends => []
