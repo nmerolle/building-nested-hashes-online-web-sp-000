045c408457559263c5dc309da148d8a6213f2366
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-09 23:16:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,10 +1,12 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {:name => "Lord Montague", :age => "53"
+      :patriarch => 
+      :name => "Lord Montague", :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Montague", :age => "54"
         #your key/value pairs here
         },
       :hero => {
