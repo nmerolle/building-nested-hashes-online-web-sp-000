a327c3045b02ebb01b4120d3a8e442bbc0863719
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-10 00:29:13 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,7 @@ def third_challenge
         :name => "Romeo", :age => "15", :status => "alive"
         #your key/value pairs here
         },
-      :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"}, {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}]
+      :hero_friends => []
    },     :patriarch => {
         :name => "Lord Capulet", :age => "50"
         #your key/value pairs here
